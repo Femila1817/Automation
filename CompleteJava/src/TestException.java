3d14064b1cdfa632813a1a6e4cdc6e799d3523d6
Femila1817 <53483514+Femila1817@users.noreply.github.com>
2019-08-07 23:29:31 +0530

    Creating another folder

--- CompleteJava/src/TestException.java
+++
@@ -1,9 +0,0 @@
-
-public class TestException extends Exception {
-
-	public TestException(String s) {
-		super(s); //this to ge the desired msg
-	} // this is the newly creating own exception -->but still not giving the desired msg. 
-
-	// SO HOW TO map this string to get that msg :super comes here
-}
